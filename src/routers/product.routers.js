1b0ebf41ea204e08b74651a3b6d64d98227e14af
Juan Felipe Bastidas Bedoya <juanfelipebastidas1105@gmail.com>
2025-12-22 20:37:53 -0500

    feat: added cloudinary function to product route

--- src/routers/product.routers.js
+++ src/routers/product.routers.js
@@ -3,15 +3,13 @@ import upload from "../middleware/uploadMiddleware.js";
 import {
   createProduct,
   getProductsByCompany,
-  updateProduct,
-  testUpload
+  updateProduct
 } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.get("/:companyId", getProductsByCompany);
-router.post("/", createProduct);
-router.put("/:productId", updateProduct); // 🔹 Nueva ruta para actualizar productos
-router.post('/test-upload', upload.single('image'), testUpload); // 🔹 Nueva ruta para pruebas de subida de archivos
+router.post("/", upload.single('image'), createProduct);
+router.put("/:productId", upload.single('image'), updateProduct); // 🔹 Nueva ruta para actualizar productos
 
 export default router;
